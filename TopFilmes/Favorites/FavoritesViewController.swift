2e532215867216f95ee9a77be5a44cfc64b17c60
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-08-17 10:31:41 -0300

    Add controllers to home, search and favorites

---
+++ TopFilmes/Favorites/FavoritesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesViewController.swift
+//  TopFilmes
+//
+//  Created by Paola Golombieski Ciarcia on 17/08/23.
+//
+
+import Foundation
---
+++ TopFilmes/Home/HomeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewController.swift
+//  TopFilmes
+//
+//  Created by Paola Golombieski Ciarcia on 17/08/23.
+//
+
+import Foundation
