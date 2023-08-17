1fe2a2168052f3b392261ab9a29cfc449d61031a
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-08-17 11:15:21 -0300

    Setup tabBar and its appearence

--- TopFilmes/Home/HomeViewController.swift
+++ TopFilmes/Home/HomeViewController.swift
@@ -5,4 +5,21 @@
 //  Created by Paola Golombieski Ciarcia on 17/08/23.
 //
 
-import Foundation
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
--- TopFilmes/Home/TabBarController.swift
+++ TopFilmes/Home/TabBarController.swift
@@ -11,5 +11,38 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
+        setupBarItem()
+    }
+
+    private func setupBarItem() {
+        setupAppearence()
+
+        let homeController = HomeViewController()
+        let homeBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house")?.withTintColor(.systemOrange))
+        homeController.tabBarItem = homeBarItem
+
+        let searchController = SearchViewController()
+        let searchBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), selectedImage: UIImage(systemName: "magnifyingglass")?.withTintColor(.systemOrange))
+        searchController.tabBarItem = searchBarItem
+
+        let favoritesController = FavoritesViewController()
+        let favoritesBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "star"), selectedImage: UIImage(systemName: "star.fill"))
+        favoritesController.tabBarItem = favoritesBarItem
+
+        let controllers = [homeController, searchController, favoritesController]
+        viewControllers = controllers
+    }
+
+    private func setupAppearence() {
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.systemOrange], for: .selected)
+        UITabBar.appearance().tintColor = .systemOrange
+
+    }
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        print("Item \(item) selected")
     }
 }
